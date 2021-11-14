74c3ed0a68077ad80e918e1cc1ef9dc58e52c08e
assassinsgreed <cwaquick@gmail.com>
2021-11-13 17:18:24 -0800

    Slide-in message, config updates

--- include/new/trainer_sliding_data.h
+++ include/new/trainer_sliding_data.h
@@ -15,32 +15,98 @@ extern const u8 gText_DefaultTrainerDynamaxMsg[];
 extern const u8 gText_TestTrainerDynamaxMsg[];
 
 // Rival battle 1
-extern const u8 sText_Rival1LastSwitchIn[];
-extern const u8 sText_Rival1LowHP[];
+extern const u8 sText_Rival1_LastSwitchIn[];
+extern const u8 sText_Rival1_LowHP[];
 
 // Rival battle n
 
 // Gym Leader 1 (Terrence)
+extern const u8 sText_Leader1_FirstMonDown[];
+extern const u8 sText_Leader1_LastSwitchIn[];
+extern const u8 sText_Leader1_LowHP[];
 
-// Gym Leader 2 ()
+// Gym Leader 2 (Stacy)
+extern const u8 sText_Leader2_FirstMonDown[];
+extern const u8 sText_Leader2_LastSwitchIn[];
+extern const u8 sText_Leader2_LowHP[];
 
-// Gym Leader 3
+// Gym Leader 3 (Wendy)
+extern const u8 sText_Leader3_FirstMonDown[];
+extern const u8 sText_Leader3_LastSwitchIn[];
+extern const u8 sText_Leader3_LowHP[];
 
-// Gym Leader 4
+// Gym Leader 4 (Monty)
+extern const u8 sText_Leader4_FirstMonDown[];
+extern const u8 sText_Leader4_LastSwitchIn[];
+extern const u8 sText_Leader4_LowHP[];
 
-// Gym Leader 5
+// Gym Leader 5 (Chandler)
+extern const u8 sText_Leader5_FirstMonDown[];
+extern const u8 sText_Leader5_LastSwitchIn[];
+extern const u8 sText_Leader5_LowHP[];
 
-// Gym Leader 6
+// Gym Leader 6 (Abby)
+extern const u8 sText_Leader6_FirstMonDown[];
+extern const u8 sText_Leader6_LastSwitchIn[];
+extern const u8 sText_Leader6_LowHP[];
 
-// Gym Leader 7
+// Gym Leader 7 (Iris)
+extern const u8 sText_Leader7_FirstMonDown[];
+extern const u8 sText_Leader7_LastSwitchIn[];
+extern const u8 sText_Leader7_LowHP[];
 
-// Gym Leader 8
+// Gym Leader 8 (Dennis & Dee)
+extern const u8 sText_Leader8_FirstMonDown[];
+extern const u8 sText_Leader8_LastSwitchIn[];
+extern const u8 sText_Leader8_LowHP[];
 
-// Team Pluto n
+// Team Pluto Admin Irene
+extern const u8 sText_Irene_FirstMonDown[];
+extern const u8 sText_Irene_LastSwitchIn[];
+extern const u8 sText_Irene_LowHP[];
+
+// Team Pluto Admin Ronald
+extern const u8 sText_Roland_FirstMonDown[];
+extern const u8 sText_Roland_LastSwitchIn[];
+extern const u8 sText_Roland_LowHP[];
+
+// Team Pluto Leader Kurtis
+extern const u8 sText_Kurtis_FirstMonDown[];
+extern const u8 sText_Kurtis_LastSwitchIn[];
+extern const u8 sText_Kurtis_LowHP[];
 
 // Pokemon Trainer Alistair
+extern const u8 sText_Alistair_FirstMonDown[];
+extern const u8 sText_Alistair_LastSwitchIn[];
+extern const u8 sText_Alistair_LowHP[];
+
+// Elite Four Hannah
+extern const u8 sText_Hannah_FirstMonDown[];
+extern const u8 sText_Hannah_LastSwitchIn[];
+extern const u8 sText_Hannah_LowHP[];
+
+// Elite Four Liam
+extern const u8 sText_Liam_FirstMonDown[];
+extern const u8 sText_Liam_LastSwitchIn[];
+extern const u8 sText_Liam_LowHP[];
+
+// Elite Four Jasmine
+extern const u8 sText_Jasmine_FirstMonDown[];
+extern const u8 sText_Jasmine_LastSwitchIn[];
+extern const u8 sText_Jasmine_LowHP[];
+
+// Elite Four Thomas
+extern const u8 sText_Thomas_FirstMonDown[];
+extern const u8 sText_Thomas_LastSwitchIn[];
+extern const u8 sText_Thomas_LowHP[];
 
 // Champion Selene
-extern const u8 sText_ChampionFirstMonDown[];
-extern const u8 sText_ChampionLastSwitchIn[];
-extern const u8 sText_ChampionLowHP[];
+extern const u8 sText_Champion_FirstMonDown[];
+extern const u8 sText_Champion_LastSwitchIn[];
+extern const u8 sText_Champion_LowHP[];
+
+// Champion Selene (Rematch)
+extern const u8 sText_ChampionRematch_FirstMonDown[];
+extern const u8 sText_ChampionRematch_LastSwitchIn[];
+extern const u8 sText_ChampionRematch_LowHP[];
+
