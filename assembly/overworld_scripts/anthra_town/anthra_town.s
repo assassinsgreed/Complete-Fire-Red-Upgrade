9f5110945df0377c84a24ca34a0f4ab1d6ecf2c3
assassinsgreed <cwaquick@gmail.com>
2022-03-11 15:16:41 -0800

    Move NPC events from game to CFRU

---
+++ assembly/overworld_scripts/anthra_town/anthra_town.s
@@ -0,0 +1,15 @@
+.thumb
+.align 2
+
+.include "../xse_commands.s"
+.include "../xse_defines.s"
+
+.global EventScript_AnthraTown_FlowerGirl
+EventScript_AnthraTown_FlowerGirl:
+    msgbox gText_AnthraTown_FlowerGirl MSG_FACE
+    end
+
+.global EventScript_AnthraTown_OldMan
+EventScript_AnthraTown_OldMan:
+    msgbox gText_AnthraTown_OldMan MSG_FACE
+    end
