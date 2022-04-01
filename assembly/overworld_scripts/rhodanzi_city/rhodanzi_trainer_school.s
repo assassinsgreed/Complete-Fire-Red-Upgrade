43b94e8df2f41693a1e9d3f8727a1523a5b1884f
assassinsgreed <cwaquick@gmail.com>
2022-04-01 15:23:42 -0700

    Bug fixes in type trainer quiz

--- assembly/overworld_scripts/rhodanzi_city/rhodanzi_trainer_school.s
+++ assembly/overworld_scripts/rhodanzi_city/rhodanzi_trainer_school.s
@@ -3,6 +3,7 @@
 
 .include "../xse_commands.s"
 .include "../xse_defines.s"
+.include "../asm_defines.s"
 
 EndMovementAndEvent:
     waitmovement ALLEVENTS
@@ -56,46 +57,94 @@ EventScript_RhodanziTrainerSchool_MainRoom_Professor:
 .global EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent
 EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent:
     @ TODO: Document flag usage. Reference this script. We can overwrite some FR flags since they're not used
+    lock
+    faceplayer
     checkflag 0x230
     if FALSE _call EventScript_TypeStudentQuiz
+    checkflag 0x230
     if TRUE _call EventScript_TypeStudentQuizComplete
+    applymovement 0x1 m_LookUp
+    waitmovement ALLEVENTS
+    release
+    end
 
 EventScript_TypeStudentQuiz:
-    applymovement 0x1 m_LookLeft
-    waitmovement ALLEVENTS
     msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizIntro MSG_YESNO
     compare LASTRESULT YES
-    if YES _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestion
-    if NO _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizRejected
-    end
+    if NO _goto EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizRejected
+    call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestion
+    return
 
 EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestion:
     msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizAccepted MSG_FACE
     compare 0x4031 0 @ Grass
-    if TRUE msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_GrassQuestion MSG_KEEPOPEN
+    if TRUE _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestionGrass
     compare 0x4031 1 @ Fire
-    if TRUE msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_FireQuestion MSG_KEEPOPEN
+    if TRUE _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestionFire
     compare 0x4031 2 @ Water
-    if TRUE msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_WaterQuestion MSG_KEEPOPEN
+    if TRUE _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestionWater
+    return
+
+EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizChoices:
     multichoiceoption gText_TypeGrass 0
 	multichoiceoption gText_TypeFire 1
 	multichoiceoption gText_TypeWater 2
-    multichoice 0x60 0x0 THREE_MULTICHOICE_OPTIONS 0x0
-    @ TODO: Complete. Need to check LASTRESULT and var 4031 to see if answer is correct
-    end
+    multichoice 0x60 0x0 THREE_MULTICHOICE_OPTIONS 0x1
+    copyvar 0x40FE LASTRESULT
+    return
+
+EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestionGrass:
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_GrassQuestion MSG_KEEPOPEN
+    call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizChoices
+    compare LASTRESULT 2
+    if notequal _goto EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizFail
+    if equal _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizSuccess
+    @ TODO: Obtain item textboxes are a little broken
+    obtainitem ITEM_MIRACLE_SEED 0x1
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_ItemInfo MSG_FACE
+    return
+
+EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestionFire:
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_FireQuestion MSG_KEEPOPEN
+    call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizChoices
+    compare LASTRESULT 0
+    if notequal _goto EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizFail
+    if equal _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizSuccess
+    obtainitem ITEM_CHARCOAL 0x1
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_ItemInfo MSG_FACE
+    return
+
+EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizQuestionWater:
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_WaterQuestion MSG_KEEPOPEN
+    call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizChoices
+    compare LASTRESULT 0
+    if notequal _goto EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizFail
+    if equal _call EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizSuccess
+    obtainitem ITEM_MYSTIC_WATER 0x1
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_ItemInfo MSG_FACE
+    return
+
+EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizSuccess:
+    @ TODO: Sound effects aren't playing in these functions for some reason
+    playse 0x19 @ SE_CORRECT
+    waitse
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizSuccess MSG_FACE
+    setflag 0x230
+    return
+
+EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizFail:
+    playse 0x1A @ SE_ERROR
+    waitse
+    msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizFail MSG_FACE
+    return
 
 EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizRejected:
     msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizRejected MSG_FACE
-    end
-
-EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizSuccess:
-    @TODO: Populate. Needs to call waitfanfare and give the right item, based on 0x4031
-    end
-
-EventScript_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizFail:
-    @TODO: Populate
+    applymovement 0x1 m_LookUp
+    waitmovement ALLEVENTS
+    release
     end
 
 EventScript_TypeStudentQuizComplete:
     msgbox gText_RhodanziTrainerSchool_BasicCourse_TypeStudent_QuizComplete MSG_FACE
-    end
+    return
