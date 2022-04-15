ed7541d15138673769048f6e48547b3dadd9498a
assassinsgreed <cwaquick@gmail.com>
2022-04-15 06:30:59 -0700

    Fix mom by moving her script to an injectable one

--- assembly/overworld_scripts/anthra_town/anthra_town.s
+++ assembly/overworld_scripts/anthra_town/anthra_town.s
@@ -13,3 +13,32 @@ EventScript_AnthraTown_FlowerGirl:
 EventScript_AnthraTown_OldMan:
     msgbox gText_AnthraTown_OldMan MSG_FACE
     end
+
+.global EventScript_AnthraTown_MomMain
+EventScript_AnthraTown_MomMain:
+	lock
+	faceplayer
+	checkflag 0x258
+	if TRUE _goto EventScript_AnthraTown_MomRestPrompt
+	msgbox gText_AnthraTown_MomLeaveHome MSG_KEEPOPEN
+	closeonkeypress
+	applymovement 0x1 m_LookLeft
+	waitmovement 0x0
+	release
+	end
+
+EventScript_AnthraTown_MomRestPrompt:
+	msgbox gText_AnthraTown_MomRestPrompt MSG_KEEPOPEN
+	closeonkeypress
+	call EventScript_AnthraTown_MomRestAnimation
+	msgbox gText_AnthraTown_MomPokemonAreHealthy MSG_KEEPOPEN
+	release
+	end
+
+EventScript_AnthraTown_MomRestAnimation:
+	fadescreen 0x1
+	fanfare 0x100
+	waitfanfare
+	special 0x0
+	fadescreen 0x0
+	return
