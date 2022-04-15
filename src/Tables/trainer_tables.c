6faab7c0ecac6cdadb7762a22061c755d49f3e67
assassinsgreed <cwaquick@gmail.com>
2022-04-15 05:48:03 -0700

    injected trainer example

--- src/Tables/trainer_tables.c
+++ src/Tables/trainer_tables.c
@@ -6,72 +6,32 @@
 #include "../../include/constants/battle_ai.h"
 #include "../../include/easy_text.h"
 
-const struct TrainerMonNoItemDefaultMoves sParty_RivalLabFire[] = {
+// See battle.h for a list of struct types that can be used
+const struct TrainerMonNoItemDefaultMoves sParty_Route1_YoungsterPeter[] = {
     {
         .iv = 0,
-        .lvl = 5,
-        .species = SPECIES_TORCHIC,
-    },
-};
-
-const struct TrainerMonNoItemDefaultMoves sParty_RivalLabGrass[] = {
-    {
-        .iv = 0,
-        .lvl = 5,
-        .species = SPECIES_TREECKO,
-    },
-};
-
-const struct TrainerMonNoItemDefaultMoves sParty_RivalLabWater[] = {
-    {
-        .iv = 0,
-        .lvl = 5,
-        .species = SPECIES_MUDKIP,
+        .lvl = 3,
+        .species = SPECIES_BIDOOF,
     },
 };
 
 #define NO_NAME {_END, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE}
-#define RIVAL_NAME {_G, _a, _r, _y, _END}
 
 
 const struct Trainer gTrainers[] = {
     [TRAINER_NONE] = {
         .trainerName = NO_NAME,
     },
-    [TRAINER_RIVAL_OAKS_LAB_CHARMANDER] = {
+    [TRAINER_ROUTE_1_YOUNGSTER_PETER] = {
         .partyFlags = 0,
-        .trainerClass = CLASS_BEAUTY,
-        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
-        .trainerPic = TRAINER_PIC_BLUE,
-        .trainerName = RIVAL_NAME,
+        .trainerClass = CLASS_YOUNGSTER,
+        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_MALE,
+        .trainerPic = TRAINER_PIC_YOUNGSTER,
+        .trainerName = {_P, _e, _t, _e, _r, _END},
         .items = {},
         .doubleBattle = FALSE,
         .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE | AI_SCRIPT_CHECK_GOOD_MOVE,
-        .partySize = NELEMS(sParty_RivalLabFire),
-        .party = {.NoItemDefaultMoves = sParty_RivalLabFire}
+        .partySize = NELEMS(sParty_Route1_YoungsterPeter),
+        .party = {.NoItemDefaultMoves = sParty_Route1_YoungsterPeter}
     },
-    [TRAINER_RIVAL_OAKS_LAB_BULBASAUR] = {
-        .partyFlags = 0,
-        .trainerClass = CLASS_BEAUTY,
-        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
-        .trainerPic = TRAINER_PIC_BLUE,
-        .trainerName = RIVAL_NAME,
-        .items = {},
-        .doubleBattle = FALSE,
-        .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
-        .partySize = NELEMS(sParty_RivalLabGrass),
-        .party = {.NoItemDefaultMoves = sParty_RivalLabGrass}
-    },
-    [TRAINER_RIVAL_OAKS_LAB_SQUIRTLE] = {
-        .partyFlags = 0,
-        .trainerClass = CLASS_BEAUTY,
-        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
-        .trainerPic = TRAINER_PIC_BLUE,
-        .trainerName = RIVAL_NAME,
-        .items = {},
-        .doubleBattle = FALSE,
-        .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
-        .partySize = NELEMS(sParty_RivalLabWater),
-        .party = {.NoItemDefaultMoves = sParty_RivalLabWater}
-    }
 };
