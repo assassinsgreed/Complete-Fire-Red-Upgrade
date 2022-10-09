a3328754bfacf83f1f110e5fb35222053e39c30a
Collin <cwaquick@gmail.com>
2022-10-09 13:30:51 -0500

    Correct bike addition

--- assembly/overworld_scripts/rhodanzi_city/rhodanzi_overworld.s
+++ assembly/overworld_scripts/rhodanzi_city/rhodanzi_overworld.s
@@ -3,7 +3,6 @@
 
 .include "../xse_commands.s"
 .include "../xse_defines.s"
-.include "../asm_defines.s"
 
 .global MapScript_RhodanziOverworld_FlightSpot
 MapScript_RhodanziOverworld_FlightSpot:
@@ -14,7 +13,6 @@ MapEntryScript_RhodanziOverworld_FlightFlag:
     setworldmapflag 0x892
     sethealingplace 0x03 @ Originally Pewter City
     checkflag 0x820
-    additem ITEM_MACH_BIKE
     IF SET _call HideThugs
     end
 
