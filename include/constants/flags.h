247ca3e52cc7f62fa1b9d533cb3d2041d0f04ec6
Collin <cwaquick@gmail.com>
2023-04-02 19:09:49 -0500

    Initial Route 5 mapping

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -58,8 +58,8 @@
 #define FLAG_HIDE_RIVAL_AND_IRENE_IN_RUBARR                0x034
 #define FLAG_HIDE_ROUTE_4_BOULDERS_AND_NPCS                0x035
 #define FLAG_HIDE_ROUTE_4_FOREMAN                          0x036
-#define FLAG_HIDE_SILPH_SCOPE                              0x037
-#define FLAG_HIDE_HIDEOUT_GIOVANNI                         0x038
+#define FLAG_USED_ALREADY???                               0x037
+#define FLAG_HIDE_ROUTE_5_BIKERS                           0x038
 #define FLAG_HIDE_TOWN_MAP                                 0x039
 #define FLAG_HIDE_POKEDEX                                  0x03A
 #define FLAG_HIDE_CERULEAN_ROCKET                          0x03B
