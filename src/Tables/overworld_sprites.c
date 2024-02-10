0b86b36444986b51978fe57705dc6e55273a444e
Collin <cwaquick@gmail.com>
2024-02-10 17:28:41 -0600

    Animate Male Nidoran's OW sprite

--- src/Tables/overworld_sprites.c
+++ src/Tables/overworld_sprites.c
@@ -56,6 +56,7 @@ extern const u8 ArticunoGTiles[];
 extern const u8 GlastrierTiles[];
 extern const u8 SkierTiles[];
 extern const u8 WingullTiles[];
+extern const u8 NidoranMaleTiles[];
 
 #define EVENT_OBJ_PAL_TAG_NONE 0x1102
 #define gEventObjectBaseOam_16x16 ((const struct OamData*) 0x83A36F8)
@@ -691,6 +692,19 @@ static const struct SpriteFrameImage Table_Wingull[] =
     overworld_frame(WingullTiles, 2, 2, 6),
     overworld_frame(WingullTiles, 2, 2, 7),
     overworld_frame(WingullTiles, 2, 2, 8),
+};
+
+static const struct SpriteFrameImage Table_NidoranMale[] =
+{
+    overworld_frame(NidoranMaleTiles, 2, 2, 0),
+    overworld_frame(NidoranMaleTiles, 2, 2, 1),
+    overworld_frame(NidoranMaleTiles, 2, 2, 2),
+    overworld_frame(NidoranMaleTiles, 2, 2, 3),
+    overworld_frame(NidoranMaleTiles, 2, 2, 4),
+    overworld_frame(NidoranMaleTiles, 2, 2, 5),
+    overworld_frame(NidoranMaleTiles, 2, 2, 6),
+    overworld_frame(NidoranMaleTiles, 2, 2, 7),
+    overworld_frame(NidoranMaleTiles, 2, 2, 8),
 };
 
 // #region MC
@@ -1672,5 +1686,25 @@ const struct EventObjectGraphicsInfo GraphicsInfo_Wingull =
     .subspriteTables = gEventObjectSpriteOamTables_16x16,
     .anims = gEventObjectImageAnimTable_Standard,
     .images = Table_Wingull,
+    .affineAnims = gDummySpriteAffineAnimTable,
+};
+
+const struct EventObjectGraphicsInfo GraphicsInfo_NidoranMale =
+{
+    .tileTag = 0xFFFF,
+    .paletteTag1 = 0x1229,
+    .paletteTag2 = EVENT_OBJ_PAL_TAG_NONE,
+    .size = (16 * 16) / 2,
+    .width = 16,
+    .height = 16,
+    .shadowSize = SHADOW_SIZE_S,
+    .inanimate = FALSE,
+    .disableReflectionPaletteLoad = FALSE,
+    .tracks = TRACKS_NONE,
+    .gender = MALE,
+    .oam = gEventObjectBaseOam_16x16,
+    .subspriteTables = gEventObjectSpriteOamTables_16x16,
+    .anims = gEventObjectImageAnimTable_Standard,
+    .images = Table_NidoranMale,
     .affineAnims = gDummySpriteAffineAnimTable,
 };
