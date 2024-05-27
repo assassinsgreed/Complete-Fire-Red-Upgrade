7cec2c4b343283446b7725ee174f4bae3bdffd6f
Collin <cwaquick@gmail.com>
2024-05-26 20:33:34 -0500

    Add Sensei and Disciple base IVs

--- src/Tables/trainers_with_evs_table.h
+++ src/Tables/trainers_with_evs_table.h
@@ -1484,6 +1484,8 @@ const u8 gBaseIVsByTrainerClass[NUM_TRAINER_CLASSES] =
 	[CLASS_RUIN_MANIAC] = 10,
 	[CLASS_LADY] =  20,
 	[CLASS_PAINTER] =  10,
+	[CLASS_SENSEI] = 31,
+	[CLASS_DISCIPLE] = 25
 };
 #else //For Pokemon Unbound
 const u8 gBaseIVsByTrainerClass[NUM_TRAINER_CLASSES] =
