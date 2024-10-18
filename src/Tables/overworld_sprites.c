e6167c0745f8ff6a5088a1f31d4a77056c9609ba
Collin <cwaquick@gmail.com>
2024-10-18 12:18:03 -0500

    Add sprites for E4 Thomas

--- src/Tables/overworld_sprites.c
+++ src/Tables/overworld_sprites.c
@@ -41,6 +41,7 @@ extern const u8 LeaderDeeTiles[];
 extern const u8 E4HannahTiles[];
 extern const u8 E4LucasTiles[];
 extern const u8 E4JennaTiles[];
+extern const u8 E4ThomasTiles[];
 
 // Overworld Events
 extern const u8 SandygastTiles[];
@@ -589,6 +590,19 @@ static const struct SpriteFrameImage Table_E4Jenna[] =
     overworld_frame(E4JennaTiles, 2, 4, 6),
     overworld_frame(E4JennaTiles, 2, 4, 7),
     overworld_frame(E4JennaTiles, 2, 4, 8),
+};
+
+static const struct SpriteFrameImage Table_E4Thomas[] =
+{
+    overworld_frame(E4ThomasTiles, 2, 4, 0),
+    overworld_frame(E4ThomasTiles, 2, 4, 1),
+    overworld_frame(E4ThomasTiles, 2, 4, 2),
+    overworld_frame(E4ThomasTiles, 2, 4, 3),
+    overworld_frame(E4ThomasTiles, 2, 4, 4),
+    overworld_frame(E4ThomasTiles, 2, 4, 5),
+    overworld_frame(E4ThomasTiles, 2, 4, 6),
+    overworld_frame(E4ThomasTiles, 2, 4, 7),
+    overworld_frame(E4ThomasTiles, 2, 4, 8),
 };
 
 // Overworld events
@@ -1662,6 +1676,26 @@ const struct EventObjectGraphicsInfo GraphicsInfo_E4Jenna =
     .subspriteTables = gEventObjectSpriteOamTables_16x32,
     .anims = gEventObjectImageAnimTable_Standard,
     .images = Table_E4Jenna,
+    .affineAnims = gDummySpriteAffineAnimTable,
+};
+
+const struct EventObjectGraphicsInfo GraphicsInfo_E4Thomas =
+{
+    .tileTag = 0xFFFF,
+    .paletteTag1 = 0x123A,
+    .paletteTag2 = EVENT_OBJ_PAL_TAG_NONE,
+    .size = (16 * 32) / 2,
+    .width = 16,
+    .height = 32,
+    .shadowSize = SHADOW_SIZE_M,
+    .inanimate = FALSE,
+    .disableReflectionPaletteLoad = FALSE,
+    .tracks = TRACKS_FOOT,
+    .gender = MALE,
+    .oam = gEventObjectBaseOam_16x32,
+    .subspriteTables = gEventObjectSpriteOamTables_16x32,
+    .anims = gEventObjectImageAnimTable_Standard,
+    .images = Table_E4Thomas,
     .affineAnims = gDummySpriteAffineAnimTable,
 };
 
